bf9474f9f9be8aa11ece7287d5fd091377bcbdd4
mados <dasomee@outlook.com>
2022-07-09 03:31:32 -0400

    Change grid color when hovered

--- script.js
+++ script.js
@@ -4,17 +4,14 @@ row.classList.add('row');
 const square = document.createElement('div');
 square.classList.add('square');
 
-
-console.log(row.contains);
-
-let gridSize = 32;
+let gridSize = 16;
 let containerWidth = 600;
 let pixelSize = containerWidth/gridSize;
+
 row.style.width = `${gridSize*pixelSize}px`;
 row.style.height = `${pixelSize}px`;
 square.style.width = `${pixelSize}px`
 square.style.height = `${pixelSize}px`
-
 
 for (let i=0; i<gridSize; i++){
     let gridRow = row.cloneNode(true);
