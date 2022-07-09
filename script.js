b98fc142dbc0e9b99d52f34aa7b783e2dffbcab8
mados <dasomee@outlook.com>
2022-07-09 03:17:08 -0400

    Adjust row height/width automatically by gridSize

--- script.js
+++ script.js
@@ -4,9 +4,17 @@ row.classList.add('row');
 const square = document.createElement('div');
 square.classList.add('square');
 
+
 console.log(row.contains);
 
-let gridSize = 16;
+let gridSize = 32;
+let containerWidth = 600;
+let pixelSize = containerWidth/gridSize;
+row.style.width = `${gridSize*pixelSize}px`;
+row.style.height = `${pixelSize}px`;
+square.style.width = `${pixelSize}px`
+square.style.height = `${pixelSize}px`
+
 
 for (let i=0; i<gridSize; i++){
     let gridRow = row.cloneNode(true);
