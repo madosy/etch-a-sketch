60342276e29ff6780a08dd18907fa6e7081f6524
madosy <dasomee@outlook.com>
2022-07-09 02:11:51 -0400

    Add loop for creating 16x16 grid

---
+++ script.js
@@ -0,0 +1,16 @@
+const container = document.querySelector('#container');
+const row = document.createElement('div');
+row.classList.add('row');
+const square = document.createElement('div');
+square.classList.add('square');
+
+console.log(row.contains)
+
+
+for (let i=0; i<16; i++){
+    let gridRow = row;
+    for (let j=0; j<16; j++){
+        gridRow.appendChild(square)
+    }
+    container.appendChild(gridRow)
+}
