06204b3d6b67b74200fe16653bc19f1e3b4522f1
madosy <dasomee@outlook.com>
2022-07-19 11:58:01 -0400

    Use flex for row item alignment

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
-let root = document.querySelector(':root');
-
+// Create Grid Components
 const container = document.querySelector('#container');
 const row = document.createElement('div');
 row.classList.add('row');
 const square = document.createElement('div');
 square.classList.add('square');
 
+// User Input Parameters
+let colorSelected = '#8785A2'
 let gridSize = 64;
-let containerWidth = 501;
-let pixelSize = Math.floor(containerWidth / gridSize * 100) / 100;
 
+// Resize Grid Squares
+let containerWidth = 480;
+let squareWidth = Math.floor(containerWidth / gridSize);
+let root = document.querySelector(':root');
+root.style.setProperty('--sqwidth',`${squareWidth}px`);
+root.style.setProperty('--sqheight',`${squareWidth}px`);
+root.style.setProperty('--row_width',`${containerWidth}px`);
 
-root.style.setProperty('--sqwidth',`${pixelSize}px`);
-root.style.setProperty('--sqheight',`${pixelSize}px`);
-root.style.setProperty('--row_width',`${pixelSize*gridSize}px`);
-// square.style.height = `${pixelSize}px`
-
+// Assemble Grid
 for (let i = 0; i < gridSize; i++) {
     let gridRow = row.cloneNode(true);
     gridRow.setAttribute('id', `r${i}`)
@@ -28,26 +30,15 @@ for (let i = 0; i < gridSize; i++) {
 }
 
 const grid = document.querySelectorAll('.square');
-// grid.forEach(square => square.addEventListener('click',console.log(`Hi, I'm ${square.id}`)))
-
-let colorSelected = '#8785A2'
 
 grid.forEach((square) => square.addEventListener('mousedown', (theEvent) => {
-    if (theEvent.buttons == 1) {
+    
         console.log("start!");
         theEvent.target.style.backgroundColor = colorSelected;
         startDrawing();
-    }
+    
 }));
-// ));
 
-// grid.forEach((square) => square.addEventListener('mouseup', () => {
-//     console.log('stop!')
-//     stopDrawing();
-// }
-// ));
-
-// grid.forEach(square => square.removeEventListener('mouseover'))
 
 
 var startDrawing = function () {
@@ -55,7 +46,6 @@ var startDrawing = function () {
 }
 
 var changeColor = function () {
-    // console.log(this.id);
     this.style.backgroundColor = colorSelected;
 }
 
@@ -67,10 +57,8 @@ const body = document.querySelector('body');
 body.addEventListener('mousedown', (theEvent) => {
     if (theEvent.buttons == 1) {
         console.log('body')
-        console.log(square)
         console.log(colorSelected)
         square.style.backgroundColor = colorSelected;
-        console.log(square)
         startDrawing();
     }
 });
@@ -79,10 +67,3 @@ body.addEventListener('mouseup', () => {
     console.log('stop!')
     stopDrawing();
 });
-
-// body.addEventListener('mouseover', (e) => console.log(e));
-// body.addEventListener('mouseup', (e) => console.log(e));
-// body.addEventListener('mousedown', (myEvent) => {
-//     console.log(myEvent.buttons);
-//     console.log(myEvent.type);
-// });
